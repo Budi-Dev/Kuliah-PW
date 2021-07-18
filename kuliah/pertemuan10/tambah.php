c468694586f59f0bd31b94c3abe462ab05eaa67f
BUDI-TAISS <budi955858@gmail.com>
2021-07-18 14:09:51 +0700

    update tambah.php

--- kuliah/pertemuan10/tambah.php
+++ kuliah/pertemuan10/tambah.php
@@ -1,7 +1,7 @@
 <?php
 require 'functions.php';
 
-// cek apakah tombol sudah ditekan
+// cek apakah tombol  sudah ditekan
 if (isset($_POST['submit'])) {
     if (tambah($_POST) > 0) {
         echo "<script>
@@ -22,31 +22,38 @@ if (isset($_POST['submit'])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tambah Data Mahasiswa</title>
+    <style>
+        table tr {
+            text-align: left;
+        }
+    </style>
 </head>
 
 <body>
     <h3>Form Tambah Data</h3>
+    <a href="latihan3.php">Kembali ke Daftar Mahasiswa</a>
+    <br><br>
     <form action="" method="post">
         <table>
             <tr>
-                <td><label for='nama'>Nama</label></td>
-                <td>: <input type='text' name='nama' id='nama' autofocus required></td>
+                <th><label for='nama'>Nama</label></th>
+                <th>: <input type='text' name='nama' id='nama' autofocus required></th>
             </tr>
             <tr>
-                <td><label for='nrp'>NRP</label></td>
-                <td>: <input type='text' name='nrp' id='nrp' required></td>
+                <th><label for='nrp'>NRP</label></th>
+                <th>: <input type='text' name='nrp' id='nrp' required></th>
             </tr>
             <tr>
-                <td><label for='email'>Email</label></td>
-                <td>: <input type='text' name='email' id='email' required></td>
+                <th><label for='email'>Email</label></th>
+                <th>: <input type='text' name='email' id='email' required></th>
             </tr>
             <tr>
-                <td><label for='jurusan'>Jurusan</label></td>
-                <td>: <input type='text' name='jurusan' id='jurusan' required></td>
+                <th><label for='jurusan'>Jurusan</label></th>
+                <th>: <input type='text' name='jurusan' id='jurusan' required></th>
             </tr>
             <tr>
-                <td><label for='gambar'>Gambar</label></td>
-                <td>: <input type='text' name='gambar' id='gambar' required></td>
+                <th><label for='gambar'>Gambar</label></th>
+                <th>: <input type='text' name='gambar' id='gambar' required></th>
             </tr>
             <tr>
                 <td><button type='submit' name='submit' value='oke' id='submit'>Tambah Data!</button></td>
